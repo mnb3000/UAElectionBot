866ba3952fdd6451c771132265f421aad38d9489
mnb3000 <misha.mykolyshyn@gmail.com>
2019-04-01 00:58:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const { JSDOM } = require('jsdom');
+const TelegramBot = require('node-telegram-bot-api');
+
+async function getResults() {
+  const dom = await JSDOM.fromURL("https://cvk.gov.ua/pls/vp2019/wp300pt001f01=719.html");
+  const { document } = dom.window;
+  const allTables = document.querySelectorAll('table');
+  const firstTableRows = allTables.item(0).rows;
+  const processedPercent = parseFloat(firstTableRows.item(0).cells.item(1).textContent.trim());
+  const voteCount = parseInt(firstTableRows.item(1).cells.item(1).textContent.trim().replace(' ', ''), 10);
+  const invalidPercent = parseFloat(firstTableRows.item(2).cells.item(1).textContent.trim());
+
+  const secondTableRows = allTables.item(1).rows;
+  const candidates = [];
+  for (let row of secondTableRows) {
+    const { cells } = row;
+    if (cells.item(0).textContent === 'Кандидат') continue;
+    candidates.push({
+      name: cells.item(0).textContent,
+      percent: parseFloat(cells.item(2).textContent.trim().replace(' ', '')),
+      count: parseInt(cells.item(3).textContent.trim().replace(' ', ''), 10),
+    });
+  }
+
+  return {
+    processedPercent,
+    voteCount,
+    invalidPercent,
+    candidates
+  }
+}
+
+async function formatMessage(candidateCount = 0) {
+  const results = await getResults();
+  let response = `*${results.processedPercent}%* протоколов\n*${results.voteCount}* голосов\n*${results.invalidPercent}%* бюллетеней недействительны\n\n`;
+  results.candidates.slice(0, candidateCount ? candidateCount : undefined).forEach((candidate) => {
+    response += `*${candidate.name}* - ${candidate.percent}%  _(${candidate.count} голосов)_\n`
+  });
+  return response
+}
+
+const bot = new TelegramBot('846188647:AAGyAw0MWKBhV6BO89E9ccYE3Uxh-HGki8k', { polling: true });
+
+bot.onText(/^\/results$/, async (msg) => {
+  const response = await formatMessage(5);
+  await bot.sendMessage(msg.chat.id, response, { parse_mode: 'Markdown' });
+});
+bot.onText(/^\/results_all$/, async (msg) => {
+  const response = await formatMessage();
+  await bot.sendMessage(msg.chat.id, response, { parse_mode: 'Markdown' });
+});
+
+bot.onText(/^\/start$/, async (msg) => {
+  await bot.sendMessage(msg.chat.id, 'Привет! Я помогаю следить за обработкой результатов выборов президента Украины!\nНажми /results чтобы увидеть текущие результаты');
+});
